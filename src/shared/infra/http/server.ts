054bd7c15d2e9faabdaaf0aa02967d9fe5b1c7ab
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-02-21 21:36:21 -0300

    start github repository

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import 'reflect-metadata'
+
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import 'express-async-errors';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
+
+import routes from './routes';
+import globalErrorHandling from './middleware/globalErrorHandling';
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(routes);
+
+app.use(globalErrorHandling)
+
+server.listen(process.env.PORT || 3334, async () => {
+  /* eslint-disable no-console */
+  console.log(`🚀 Server started on port ${process.env.PORT || 3334}!`);
+});
