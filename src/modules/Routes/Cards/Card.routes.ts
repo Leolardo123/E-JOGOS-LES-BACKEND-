e11ef956d44fa17aae2fab1541764d2945afe5be
Leonardo Ferreira dos Santos <60516782+Leolardo123@users.noreply.github.com>
2022-03-30 21:18:37 -0300

    fix: Cards 2

--- src/modules/Routes/Cards/Card.routes.ts
+++ src/modules/Routes/Cards/Card.routes.ts
@@ -1,10 +1,14 @@
-import { index } from "@shared/validation/Universal.validation";
+import CardsController from "@modules/controller/CardsController";
 import { Router } from "express";
+import { create, index } from "./validation/CardRoutes.validation";
 
 const cardRouter = Router();
+const cardsController = new CardsController();
 
-cardRouter.get('/',index, function() {
+cardRouter.get('/', index, function() {
     console.log('teste')
 })
+
+cardRouter.post('/', create, cardsController.create)
 
 export default cardRouter
